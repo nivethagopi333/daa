131c9af830688b3df92ba346fd1525c2e92f86be
nivethagopi333 <123889816+nivethagopi333@users.noreply.github.com>
2023-11-24 05:29:23 +0530

    Add files via upload

---
+++ daa12.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>  
+int main() {  
+    int triangle[5][5];  
+  
+    // initialize first row to 1's  
+    for (int i = 0; i< 5; i++) {  
+        triangle[0][i] = 1;  
+    }  
+   
+    for (int i = 1; i< 5; i++) {  
+        for (int j = 0; j <= i; j++) {  
+            if (j == 0 || j == i) {  
+                triangle[i][j] = 1;  
+            } else {  
+                triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];  
+            }  
+        }  
+    }  
+  
+    for (int i = 0; i< 5; i++) {  
+        for (int j = 0; j <= i; j++) {  
+printf("%d ", triangle[i][j]);  
+        }  
+printf("\n");  
+    }  
+  
+    return 0;  
+}
